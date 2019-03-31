275d98906fb95b681a13ef5eb4e2bbd8bb0182b2
Steffan153 <mathmammoth@gmail.com>
2019-03-30 19:44:15 -0600

    set navbar to fix at the top

--- header.php
+++ header.php
@@ -3,7 +3,7 @@
     display: block;
   }
 </style>
-<nav class="navbar navbar-expand-md navbar-dark bg-primary mb-3">
+<nav class="navbar navbar-expand-md navbar-dark bg-primary fixed-top">
   <a class="navbar-brand" href="https://www.mathmammoth.com"><img src="favicon.ico" alt="MM icon" style="width: 40px; height: 40px" class="rounded mr-2">Math Mammoth</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar">
     <span class="navbar-toggler-icon"></span>
@@ -33,8 +33,8 @@
         <a class="nav-link<?php echo $page == 'findfactors' ? ' active' : ''; ?>" href="factorfind.php">Find Factors</a>
       </li>
       <li class="nav-item dropdown">
-        <a 
-          class="nav-link dropdown-toggle<?php echo $page == 'addition-single-digit' || $page == 'factfam-addsub' || $page == 'addition-two-digit' ? ' active' : ''; ?>" 
+        <a
+          class="nav-link dropdown-toggle<?php echo $page == 'addition-single-digit' || $page == 'factfam-addsub' || $page == 'addition-two-digit' ? ' active' : ''; ?>"
           href="javascript:void(0);">Add & Subtract</a>
         <div class="dropdown-menu" style="margin-top: -1px">
           <a href="addition-single-digit.php" class="dropdown-item<?php echo $page == 'addition-single-digit' ? ' active' : ''; ?>">Single-Digit</a>
@@ -45,26 +45,27 @@
     </ul>
   </div>
 </nav>
+<div style="margin-top: 100px"></div>
 <?php
-if(isset($_GET['rating'])) {
+if (isset($_GET['rating'])) {
   $email = $_GET['email'];
   $rating = $_GET['rating'];
   $feedback = $_GET['feedback'];
   $send = true;
-  if(isset($_GET['g-recaptcha-response'])){
+  if (isset($_GET['g-recaptcha-response'])) {
     $captcha = $_GET['g-recaptcha-response'];
   }
   $message = '';
-  if(!$captcha){
+  if (!$captcha) {
     $message = '<h2>Please check the captcha form.</h2>';
     $send = false;
   }
   $secretKey = "6LfiqI8UAAAAANT180ToHnW0VhyuB2gTMAnf7zmR";
   $ip = $_SERVER['REMOTE_ADDR'];
   $response = file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=" . $secretKey . "&response=" . $captcha . "&remoteip=" . $ip);
-  $responseKeys = json_decode($response,true);
-  if($message !== '<h2>Please check the captcha.</h2>') {
-    if(!intval($responseKeys["success"])) {
+  $responseKeys = json_decode($response, true);
+  if ($message !== '<h2>Please check the captcha.</h2>') {
+    if (!intval($responseKeys["success"])) {
       $message = 'You didn\'t pass the captcha.';
       $send = false;
     } else {
@@ -78,7 +79,10 @@ if(isset($_GET['rating'])) {
 
   IP: $ip
   ";
-  if ($send) mail('maria_miller@mathmammoth.com', 'Math Mammoth Practice Feedback - Rating: ' . $rating, $a,'From: ' . (trim($email) ? 'maria_miller@mathmammoth.com' : trim($email)));
+  if ($send) {
+    mail('maria_miller@mathmammoth.com', 'Math Mammoth Practice Feedback - Rating: ' . $rating, $a, 'From: ' . (trim($email) ? 'maria_miller@mathmammoth.com' : trim($email)));
+  }
+
   echo '<br><div class="container"><div class="jumbotron text-black" style="background-color: #8cfc88">' . $message . '</div></div><br>';
 }
 ?>
