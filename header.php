d591d9875bb44295e20b5fa30b7f4203b811360c
Caleb Miller <caleb@mathmammoth.com>
2019-07-04 15:12:58 -0600

    changed header

--- header.php
+++ header.php
@@ -3,7 +3,7 @@
   <button class="btn btn-primary" title="Install app?" onclick="deferredPrompt.prompt(); document.getElementById('addHomeScreen').style.display = 'none'">Install</button> 
   <button onclick="document.getElementById('addHomeScreen').style.display = 'none'" class="btn btn-danger">Not right now</a>
 </div>
-<nav class="navbar navbar-expand-lg sticky-top navbar-dark" style="z-index: 1000; background-color: #7947d0">
+<nav class="navbar navbar-expand-lg sticky-top navbar-dark" style="z-index: 1000; background-color: #712be2">
   <a class="navbar-brand" href="https://www.mathmammoth.com"><img src="favicon.ico" alt="MM icon" style="width: 40px; height: 40px" class="rounded mr-2">Math Mammoth</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar" aria-controls="navbar" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
@@ -116,7 +116,7 @@
     </ul>
   </div>
 </nav>
-<div style="margin-top: 100px"></div>
+<div style="margin-top: 50px"></div>
 <noscript>
   We're sorry, but our website requires JavaScript to run.
 </noscript>
