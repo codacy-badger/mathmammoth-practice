a53daac8d667fc3802825d706c089d0b61ca106c
Caleb Miller <caleb@mathmammoth.com>
2019-07-20 09:28:26 -0600

    update sw

--- sw.js
+++ sw.js
@@ -1,94 +1,94 @@
-var CACHE_VERSION = 99;
-var CACHE_STATIC_NAME = 'static-v' + CACHE_VERSION;
-var CACHE_DYNAMIC_NAME = 'dynamic-v' + CACHE_VERSION;
-var CACHE_IMAGES_NAME = 'images-v1';
-var CACHE_IMAGES2_NAME = 'images2-v1';
-var CACHE_SCREENSHOTS_NAME = 'screenshots-v1';
+var CACHE_VERSION = 100;
+var CACHE_STATIC_NAME = "static-v" + CACHE_VERSION;
+var CACHE_DYNAMIC_NAME = "dynamic-v" + CACHE_VERSION;
+var CACHE_IMAGES_NAME = "images-v1";
+var CACHE_IMAGES2_NAME = "images2-v1";
+var CACHE_SCREENSHOTS_NAME = "screenshots-v1";
 
-self.addEventListener('install', function(event) {
+self.addEventListener("install", function(event) {
   self.skipWaiting();
-  console.log('[Service Worker] Installing Service Worker ...', event);
+  console.log("[Service Worker] Installing Service Worker ...", event);
   event.waitUntil(
     caches.open(CACHE_STATIC_NAME).then(function(cache) {
-      console.log('[Service Worker] Precaching App Shell...');
+      console.log("[Service Worker] Precaching App Shell...");
       cache.addAll([
-        '/practice/',
-        '/practice/favicon.ico',
-        '/practice/index',
-        '/practice/practice.css',
-        '/practice/multiply-with-zeros',
-        '/practice/exponents',
-        '/practice/divide-numbers-ending-in-zeros',
-        '/practice/multiplication',
-        '/practice/fact-families',
-        '/practice/addition-single-digit',
-        '/practice/addition-subtraction-two-digit',
-        '/practice/factorfind',
-        '/practice/division',
-        '/practice/mystery-picture',
-        '/practice/mystery-picture-subtraction',
-        '/practice/mystery-picture-integers',
-        '/practice/division-remainder',
-        '/practice/fractions-decimals',
-        '/practice/rounding',
-        '/practice/place-value',
-        '/practice/app.js',
-        '/practice/bootstrap.min.css',
-        '/practice/bootstrap.min.js',
-        '/practice/jquery-3.3.1.min.js',
-        '/practice/sweetalert.min.js',
-        '/practice/mm.css',
-        '/practice/lazyload.min.js',
-        '/practice/girl_online_practice.jpg'
+        "/practice/",
+        "/practice/favicon.ico",
+        "/practice/index",
+        "/practice/practice.css",
+        "/practice/multiply-with-zeros",
+        "/practice/exponents",
+        "/practice/divide-numbers-ending-in-zeros",
+        "/practice/multiplication",
+        "/practice/fact-families",
+        "/practice/addition-single-digit",
+        "/practice/addition-subtraction-two-digit",
+        "/practice/factorfind",
+        "/practice/division",
+        "/practice/mystery-picture",
+        "/practice/mystery-picture-subtraction",
+        "/practice/mystery-picture-integers",
+        "/practice/division-remainder",
+        "/practice/fractions-decimals",
+        "/practice/rounding",
+        "/practice/place-value",
+        "/practice/app.js",
+        "/practice/bootstrap.min.css",
+        "/practice/bootstrap.min.js",
+        "/practice/jquery-3.3.1.min.js",
+        "/practice/sweetalert.min.js",
+        "/practice/mm.css",
+        "/practice/lazyload.min.js",
+        "/practice/girl_online_practice.jpg"
       ]);
     })
   );
   event.waitUntil(
     caches.open(CACHE_IMAGES_NAME).then(function(cache) {
-      console.log('[Service Worker] Precaching mystery picture images...');
+      console.log("[Service Worker] Precaching mystery picture images...");
       var images = [];
       for (var i = 0; i < 44; i++) {
-        images.push('/practice/images/' + (i + 1) + '.jpg');
+        images.push("/practice/images/" + (i + 1) + ".jpg");
       }
       cache.addAll(images);
     })
   );
   event.waitUntil(
     caches.open(CACHE_IMAGES2_NAME).then(function(cache) {
-      console.log('[Service Worker] Precaching mystery picture images 2...');
+      console.log("[Service Worker] Precaching mystery picture images 2...");
       var images = [];
       for (var i = 0; i < 34; i++) {
-        images.push('/practice/images/' + (i + 1) + '.jpg');
+        images.push("/practice/images/" + (i + 1) + ".jpg");
       }
       cache.addAll(images);
     })
   );
   event.waitUntil(
     caches.open(CACHE_SCREENSHOTS_NAME).then(function(cache) {
-      console.log('[Service Worker] Precaching screenshots');
+      console.log("[Service Worker] Precaching screenshots");
       cache.addAll([
-        '/practice/mystery-picture.jpg',
-        '/practice/mystery-picture-sub.png',
-        '/practice/mystery-picture-integers.jpg',
-        '/practice/fractions-matching.jpg',
-        '/practice/division-remainders.jpg',
-        '/practice/divide-zeros.jpg',
-        '/practice/exponents-screenshot.jpg',
-        '/practice/fact-families-screenshot.jpg',
-        '/practice/find-factors.jpg',
-        '/practice/place-value-screenshot.jpg',
-        '/practice/multiplication-screenshot.jpg',
-        '/practice/multiply-zeros.jpg',
-        '/practice/rounding-screenshot.jpg',
-        '/practice/single-digit-addition.jpg',
-        '/practice/two-digit-addition-subtraction.jpg'
+        "/practice/mystery-picture.jpg",
+        "/practice/mystery-picture-sub.png",
+        "/practice/mystery-picture-integers.jpg",
+        "/practice/fractions-matching.jpg",
+        "/practice/division-remainders.jpg",
+        "/practice/divide-zeros.jpg",
+        "/practice/exponents-screenshot.jpg",
+        "/practice/fact-families-screenshot.jpg",
+        "/practice/find-factors.jpg",
+        "/practice/place-value-screenshot.jpg",
+        "/practice/multiplication-screenshot.jpg",
+        "/practice/multiply-zeros.jpg",
+        "/practice/rounding-screenshot.jpg",
+        "/practice/single-digit-addition.jpg",
+        "/practice/two-digit-addition-subtraction.jpg"
       ]);
     })
   );
 });
 
-self.addEventListener('activate', function(event) {
-  console.log('[Service Worker] Activating Service Worker ....', event);
+self.addEventListener("activate", function(event) {
+  console.log("[Service Worker] Activating Service Worker ....", event);
   event.waitUntil(
     caches.keys().then(function(keyList) {
       return Promise.all(
@@ -100,7 +100,7 @@ self.addEventListener('activate', function(event) {
             key != CACHE_IMAGES2_NAME &&
             key != CACHE_SCREENSHOTS_NAME
           ) {
-            console.log('[Service Worker] Removing old cache...', key);
+            console.log("[Service Worker] Removing old cache...", key);
             return caches.delete(key);
           }
         })
@@ -110,7 +110,7 @@ self.addEventListener('activate', function(event) {
   return self.clients.claim();
 });
 
-self.addEventListener('fetch', function(event) {
+self.addEventListener("fetch", function(event) {
   event.respondWith(
     caches
       .match(event.request)
